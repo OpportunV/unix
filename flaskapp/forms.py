3b5dabccb1af4d2ce9ad85d5d4b2898b9dcd7014
OpportunV <Vadya.95@mail.ru>
2019-12-06 05:29:18 +0300

    actually can be used

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired
 
 from flaskapp.models import User
@@ -11,3 +11,10 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField("Log In")
+    
+
+class TaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    is_active = BooleanField('Show')
+    submit = SubmitField('Submit')
--- flaskapp/views/general.py
+++ flaskapp/views/general.py
@@ -1,9 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 
-from flaskapp.app import bcrypt
-from flaskapp.forms import LoginForm
-from flaskapp.models import User
+from flaskapp.app import bcrypt, db
+from flaskapp.forms import LoginForm, TaskForm
+from flaskapp.models import User, Task
 
 
 bp = Blueprint('general', __name__)
@@ -17,7 +17,11 @@ def before_request():
 
 @bp.route('/')
 def index():
-    return render_template('general/index.html', title='Main Page')
+    if current_user.is_admin:
+        tasks = Task.query.all()
+    else:
+        tasks = Task.query.filter_by(is_active=True)
+    return render_template('general/index.html', title='Задания', tasks=tasks)
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -44,6 +48,73 @@ def logout():
     logout_user()
     return redirect(url_for('general.login'))
 
+
+@bp.route('/new', methods=['GET', 'POST'])
+def new():
+    if not current_user.is_admin:
+        flash(f'You are not allowed to add tasks.', 'error')
+        return redirect(url_for('general.index'))
+    
+    form = TaskForm()
+    if form.validate_on_submit():
+        task = Task(title=form.title.data,
+                    content=form.content.data,
+                    is_active=form.is_active.data,
+                    author=current_user)
+        db.session.add(task)
+        db.session.commit()
+        flash('Task has been created!', 'success')
+        return redirect(url_for('general.index', link=task.link))
+    return render_template('general/new.html', title='New Task', form=form)
+
+
+@bp.route('/<link>/update', methods=['GET', 'POST'])
+def task_update(link):
+    if not current_user.is_admin:
+        flash(f'You are not allowed to edit tasks.', 'error')
+        return redirect(url_for('general.index'))
+    
+    task = Task.query.filter(Task.link == link).first()
+    
+    if not task:
+        flash('task not found', 'warning')
+        return redirect(url_for('general.index'))
+    
+    form = TaskForm()
+    
+    if form.validate_on_submit():
+        task.title = form.title.data
+        task.content = form.content.data
+        task.is_active = form.is_active.data
+        db.session.commit()
+        flash('Task has been updated.', 'success')
+        return redirect(url_for('general.index', link=task.link))
+    
+    if request.method == 'GET':
+        form.title.data = task.title
+        form.content.data = task.content
+        form.is_active.data = task.is_active
+    
+    return render_template('general/new.html', title='Update Task', form=form)
+
+
+@bp.route('/<link>/delete', methods=['POST'])
+def task_delete(link):
+    if not current_user.is_admin:
+        flash(f'You are not allowed to delete tasks.', 'error')
+        return redirect(url_for('general.index'))
+    
+    task = Task.query.filter(Task.link == link).first()
+    
+    if not task:
+        flash('task not found', 'warning')
+        return redirect(url_for('general.index'))
+    
+    db.session.delete(task)
+    db.session.commit()
+    
+    flash('Task has been deleted.', 'success')
+    return redirect(url_for('general.index'))
 
 # @bp.route('/account')
 # @login_required
